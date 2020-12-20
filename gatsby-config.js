fb1d83fd65cd3fd6d17eea6bfb41b90f41a18c68
danp-dev <65661237+danp-dev@users.noreply.github.com>
2020-12-20 14:02:40 +1030

    Update website title and description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Zippy Forms | Fast, Secure Digital Forms and Documents',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Zippy Forms makes your life easy by digitising all of your forms and documents.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
